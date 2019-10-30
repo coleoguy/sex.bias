b36ecdf35f2324199c7be9562a7c2458bb789c46
Heath Blackmon <coleoguy@gmail.com>
2019-10-30 09:56:52 -0500

    Update fitnessHD.R

--- rscripts/functions/fitnessHD.R
+++ rscripts/functions/fitnessHD.R
@@ -7,23 +7,18 @@
 # functions to assess the impact of allele frequency change in our haplodiploid model in terms of
 # feminisation or masculinisation of the genome
 
-getFitness <- function(data, dom, h, s) {
+getFitness <- function(data, dom, s) {
+  # data data frame of results
+  # dom dominance factor for female benefit allele
+  # 
   results <- matrix(, nrow(data), 2)
   colnames(results) <- c("wbar.fem", "wbar.mal")
   for (i in 1:nrow(data)) {
-    if (dom == "h=0") {
-      # we pull the frequency of the female beneficial allele and store it in "p".
-      # if you want the frequency of the male beneficial allele this is 1-p
-      p <- data[i, 1]
-      
-      # Female fitness functions
-      wf.a11 <- 1 + s
-      wf.a12 <- 1 + h * s
-      wf.a22 <- 1
-      # Male fitness functions
-      wm.a1 <- 1 / (1 + s)
-      wm.a2 <- 1
-      
+    switch(dom,
+           "h=0" = p <- data[i, 1],
+           "h=0.5" = p <- data[i, 2],
+           "h=1" = p <- data[i, 3])
+    
       # genotype frequencies
       # females
       fa11 <- p ^ 2
@@ -32,72 +27,40 @@ getFitness <- function(data, dom, h, s) {
       # males
       ma1 <- p
       ma2 <- 1 - p
+
+    if (dom == "h=0") {
+      # Female fitness functions
+      wf.a11 <- 1 + s
+      wf.a12 <- 1 
+      wf.a22 <- 1
+      # Male fitness functions
+      wm.a1 <- 1 / (1 + s)
+      wm.a2 <- 1
       
-      # Standardise
-      wbar.fem <- (((p ^ 2) * wf.a11) +
-                     ((2 * p * (1 - p)) * wf.a12) +
-                     (1 - p ^ 2 * wf.a22)) / wf.a11
-      
-      wbar.mal <- ((p * wm.a1) + (1 - p * wm.a2)) / wm.a2
     }
     if (dom == "h=0.5") {
-      # we pull the frequency of the female beneficial allele and store it in "p".
-      # if you want the frequency of the male beneficial allele this is 1-p
-      p <- data[i, 2]
-      
       # Female fitness functions
       wf.a11 <- 1 + s
-      wf.a12 <- 1 + h * s
+      wf.a12 <- 1 + .5 * s
       wf.a22 <- 1
       # Male fitness functions
       wm.a1 <- 1 / (1 + s)
-      wm.a2 <- 1
-      
-      # genotype frequencies
-      # females
-      fa11 <- p ^ 2
-      fa12 <- 2 * p * (1 - p)
-      fa22 <- (1 - p) ^ 2
-      # males
-      ma1 <- p
-      ma2 <- 1 - p
-      
-      # Standardise
-      wbar.fem <- (((p ^ 2) * wf.a11) +
-                     ((2 * p * (1 - p)) * wf.a12) +
-                     (1 - p ^ 2 * wf.a22)) / wf.a11
-      
-      wbar.mal <- ((p * wm.a1) + (1 - p * wm.a2)) / wm.a2
+      wm.a2 <- 1    
     }
     if (dom == "h=1") {
-      # we pull the frequency of the female beneficial allele and store it in "p".
-      # if you want the frequency of the male beneficial allele this is 1-p
-      p <- data[i, 3]
-      
       # Female fitness functions
       wf.a11 <- 1 + s
-      wf.a12 <- 1 + h * s
+      wf.a12 <- 1 + s
       wf.a22 <- 1
       # Male fitness functions
       wm.a1 <- 1 / (1 + s)
-      wm.a2 <- 1
-      
-      # genotype frequencies
-      # females
-      fa11 <- p ^ 2
-      fa12 <- 2 * p * (1 - p)
-      fa22 <- (1 - p) ^ 2
-      # males
-      ma1 <- p
-      ma2 <- 1 - p
-      
-      # Standardise
-      wbar.fem <- (((p ^ 2) * wf.a11) +
-                     ((2 * p * (1 - p)) * wf.a12) +
-                     (1 - p ^ 2 * wf.a22)) / wf.a11
-      
-      wbar.mal <- ((p * wm.a1) + (1 - p * wm.a2)) / wm.a2
+      wm.a2 <- 1      
     }
+    # Standardise to maximum possible fitness
+    wbar.fem <- ((fa11 * wf.a11) +
+                 (fa12 * wf.a12) +
+                 (fa22 * wf.a22)) / wf.a11
+    wbar.mal <- ((ma1 * wm.a1) + (ma2 * wm.a2)) / wm.a2
     results[i, 1:2] <- c(wbar.fem, wbar.mal)
   }
   return(as.data.frame(results))
