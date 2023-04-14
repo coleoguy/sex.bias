ee6f1b3af032c0eed40e1c417bcceca1281d9a60
Priscilla Glenn <priscilla_glenn@outlook.com>
2023-04-14 16:50:14 -0500

    Updates

--- scripts/PG-Fitnessdifference_graph.R
+++ scripts/PG-Fitnessdifference_graph.R
@@ -38,6 +38,11 @@ test <-test[test$OSR == 1.0,,]
 test <-test[test$h == 0.5,]
 
 
+test2 <-simple.mdat[simple.mdat$rd == 0.2,]
+test2 <-test2[test2$females == 1000,]
+test2 <-test2[test2$OSR == 1.0,,]
+test2 <-test2[test2$h == 0.5,]
+
 
 
 #Sex chromosome
@@ -57,12 +62,6 @@ str(mfit.sex)
 #h = 0.5
 
 #mfit.sex.5h <- mfit.sex[mfit.sex$h == 0.5,]
-
-
-#checking values "Don't run
-#mfit.sex.5h <- mfit.sex.5h[mfit.sex.5h$females == 1000,]
-#mfit.sex.5h <- mfit.sex.5h[mfit.sex.5h$OSR == 1.0,]
-
 
 
 #### Female 
@@ -92,7 +91,7 @@ ffit.sex <- simple.fsex[simple.fsex$h != 99,]
 ffit.sex$males<-as.factor(ffit.sex$males)
 ffit.sex$s<-as.factor(ffit.sex$s)
 ffit.sex$h<-as.factor(ffit.sex$h)
-str(ffit.sex)
+#str(ffit.sex)
 
 #ffit.sex.5h <- ffit.sex[ffit.sex$h == 0.5,]
 
@@ -120,7 +119,7 @@ mfit.aut <- simple.maut[simple.maut$h != 99,]
 mfit.aut$females<-as.factor(mfit.aut$females)
 mfit.aut$s<-as.factor(mfit.aut$s)
 mfit.aut$h<-as.factor(mfit.aut$h)
-str(mfit.aut)
+#str(mfit.aut)
 
 #h = 0.5
 
@@ -135,7 +134,7 @@ ffit.aut <- simple.faut[simple.faut$h != 99,]
 ffit.aut$males<-as.factor(ffit.aut$males)
 ffit.aut$s<-as.factor(ffit.aut$s)
 ffit.aut$h<-as.factor(ffit.aut$h)
-str(ffit.aut)
+#str(ffit.aut)
 
 #ffit.aut.5h <- ffit.aut[ffit.aut$h == 0.5,]
 
@@ -154,17 +153,18 @@ colnames(AutFit)[7] <- "F_Wdiff"
 #Graphs
 
 
-my_labels = c("0.50","0.25","0.00","0.25","0.50")
-my_ticks = c(-0.50,-0.25,0,0.25,0.5)
+my_labels = c("1.0","0.75","0.50","0.25","0.00","0.25","0.50","0.75","1.0")
+my_ticks = c(-1.0,-0.75,-0.50,-0.25,0,0.25,0.5, 0.75,1.0)
 
+SexFit_h <- SexFit[SexFit$h == 0.5,]
 
-SexFitnessComp <- ggplot(SexFit, aes(x = OSR))+
+SexFitnessComp <- ggplot(SexFit_h, aes(x = OSR))+
   facet_grid(h~common)+
   geom_line(aes(y=M_Wdiff, colour =s))+
   geom_point(aes(y=M_Wdiff, shape = s, colour = s))+
   geom_point(aes(y=F_Wdiff, shape = s, colour = s))+
   geom_line(aes(y=F_Wdiff, colour = s))+
-  scale_y_continuous(limits = c(-0.6,0.6), breaks = my_ticks, labels=my_labels,expand = c(0,0))+
+  scale_y_continuous(limits = c(-1,1), breaks = my_ticks, labels=my_labels,expand = c(0,0))+
   theme(axis.line.y = element_blank(), plot.margin = unit(c(1,1,1,1), "lines"))+
   ylab(expression("Fitness difference (common - rare)\n"))+
   ggtitle("Fitness, rd = 0.2, h = 0.5")
@@ -172,6 +172,12 @@ SexFitnessComp <- ggdraw() + draw_plot(SexFitnessComp) +
   draw_label("Common Male", x = 0.03, y = 0.65, angle = 90, size =10) +
   draw_label("Common Female", x = 0.03, y = 0.30, angle = 90, size =10)
 SexFitnessComp
+
+
+test4 <-SexFit_h[SexFit_h$rd == 0.2,]
+test4 <-test4[test4$common == 1000,]
+test4 <-test4[test4$OSR == 1.0,,]
+test4 <-test4[test4$h == 0.5,]
 
 AutFit_h <- AutFit[AutFit$h == 0.5,]
 
@@ -181,7 +187,7 @@ AutFitnessComp <- ggplot(AutFit_h, aes(x = OSR))+
   geom_point(aes(y=M_Wdiff, shape = s, colour = s))+
   geom_point(aes(y=F_Wdiff, shape = s, colour = s))+
   geom_line(aes(y=F_Wdiff, colour = s))+
-  scale_y_continuous(limits = c(-0.6,0.6), breaks = my_ticks, labels=my_labels,expand = c(0,0))+
+  scale_y_continuous(limits = c(-1,1.0), breaks = my_ticks, labels=my_labels,expand = c(0,0))+
   theme(axis.line.y = element_blank(), plot.margin = unit(c(1,1,1,1), "lines"))+
   ylab(expression("Fitness difference (common - rare)\n"))+
   ggtitle("Fitness, rd = 0.5, h = 0.5") 
@@ -190,6 +196,11 @@ AutFitnessComp <- ggdraw() + draw_plot(AutFitnessComp) +
   draw_label("Common Female", x = 0.03, y = 0.30, angle = 90, size =10)
 AutFitnessComp
 
+
+test3 <-AutFit_h[AutFit_h$rd == 0.5,]
+test3 <-test3[test3$common == 1000,]
+test3 <-test3[test3$OSR == 1.0,,]
+test3 <-test3[test3$h == 0.5,]
 
 
 
